9d028fd979d068d563c8aa1f782ec721482a8e00
Dmitriy Shcherbakov <dima49.sib@inbox.ru>
2024-03-02 08:23:35 +0700

    01

---
+++ MovieQuiz/Protocols/AlertPresentationDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AlertPresentationDelegate.swift
+//  MovieQuiz
+//
+//  Created by Dmitriy Shcherbakov   on 18.02.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Protocols/AlertPresentationProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  AlertPresentationProtocol.swift
+//  MovieQuiz
+//
+//  Created by Dmitriy Shcherbakov   on 18.02.2024.
+//
+
+import Foundation
